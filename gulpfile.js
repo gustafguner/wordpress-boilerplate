34f613bac9134f6faf78d49c4c213262b74dbc94
Gustaf Gunér <gustaf@guner.se>
2018-09-05 22:17:28 +0200

    livereload

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,32 @@ var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var uglifyCSS = require('gulp-uglifycss');
+var livereload = require('gulp-livereload');
 
 gulp.task('styles:dev', function () {
   return gulp.src('src/css/sass/**/*.scss', { base: './' })
     .pipe(sass())
     .pipe(concat('style.css'))
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./src'))
+    .pipe(livereload());
 });
 
 gulp.task('styles:prod', function () {
   return gulp.src('src/css/sass/**/*.scss', { base: './' })
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
+      browsers: ['last 2 versions']
     }))
     .pipe(concat('style.css'))
     .pipe(uglifyCSS())
-    .pipe(gulp.dest('.'));
+    .pipe(gulp.dest('./src'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('watch', function () {
+  livereload.listen();
   gulp.watch('./src/css/sass/**/*.scss', ['styles:dev']);
 });
 
-gulp.task('default', ['styles:dev', 'sass:watch']);
+gulp.task('default', ['watch']);
 
 gulp.task('build', ['styles:prod']);
