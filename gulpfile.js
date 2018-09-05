627ee28f5254f60ce856c3d5d877701227c39dba
Gustaf Gunér <gustaf@guner.se>
2018-09-05 21:46:29 +0200

    Uglify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var concatCss = require('gulp-concat-css');
+var concat = require('gulp-concat');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var uglifyCSS = require('gulp-uglifycss');
 
 gulp.task('styles:dev', function () {
-  return gulp.src('./css/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('src/css/sass/**/*.scss', { base: './' })
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('styles:prod', function () {
+  return gulp.src('src/css/sass/**/*.scss', { base: './' })
+    .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(concatCss('./style.css'))
+    .pipe(concat('style.css'))
+    .pipe(uglifyCSS())
     .pipe(gulp.dest('.'));
 });
- 
+
 gulp.task('sass:watch', function () {
-  gulp.watch('./css/sass/**/*.scss', ['styles:dev']);
+  gulp.watch('./src/css/sass/**/*.scss', ['styles:dev']);
 });
 
 gulp.task('default', ['styles:dev', 'sass:watch']);
+
+gulp.task('build', ['styles:prod']);
--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $('#site-header #site-nav ul#nav-menu li a .effect').each(function() {
-		$(this).attr('data-hover', $(this).html());
-	});
-});
