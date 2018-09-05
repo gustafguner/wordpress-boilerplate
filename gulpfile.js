7cdb702e36d100d89916d3054bb6b60417d5394e
Gustaf Gunér <gustaf@guner.se>
2018-09-05 22:33:26 +0200

    js, babel, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,12 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 var uglifyCSS = require('gulp-uglifycss');
 var livereload = require('gulp-livereload');
 
 gulp.task('styles:dev', function () {
-  return gulp.src('src/css/sass/**/*.scss', { base: './' })
+  return gulp.src('src/css/sass/**/*.scss', {base: './'})
     .pipe(sass())
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./src'))
@@ -18,7 +19,7 @@ gulp.task('styles:dev', function () {
 });
 
 gulp.task('styles:prod', function () {
-  return gulp.src('src/css/sass/**/*.scss', { base: './' })
+  return gulp.src('src/css/sass/**/*.scss', {base: './'})
     .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
@@ -26,6 +27,18 @@ gulp.task('styles:prod', function () {
     .pipe(concat('style.css'))
     .pipe(uglifyCSS())
     .pipe(gulp.dest('./src'));
+});
+
+gulp.task('js:prod', function () {
+  return gulp.src('src/js/**/*.js', {base: './'})
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', function () {
