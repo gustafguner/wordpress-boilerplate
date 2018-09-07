6409de69f460fbee88a336b6be91ac53c140d694
Gustaf Gunér <gustaf@guner.se>
2018-09-07 16:30:12 +0200

    Oops, wrong js file name

--- src/functions.php
+++ src/functions.php
@@ -12,7 +12,7 @@ function headerImports () {
       wp_deregister_script('jquery');
       wp_register_script('jquery', get_template_directory_uri() . '/js/lib/jquery.min.js', array(), '3.3.1');
 
-      wp_register_script('custom-script', get_template_directory_uri() . '/js/app.js', array('jquery'), '1.0.0');
+      wp_register_script('custom-script', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0');
    
       wp_enqueue_script('custom-script');
     }
